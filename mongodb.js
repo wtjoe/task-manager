40b2943517c293aba6dc2f6417deb478237bb573
Joe Robbins <joe.robbins@willowtreeapps.com>
2023-05-30 09:25:18 -0400

    first commit

---
+++ mongodb.js
@@ -0,0 +1,25 @@
+const {MongoClient, ObjectID} = require ('mongodb')
+
+const connectionURL = 'mongodb://127.0.0.1:27017'
+const databaseName = 'task-manager'
+
+MongoClient.connect(connectionURL, { useNewUrlParser: true, useUnifiedTopology: true }, (error, client) => {
+
+    if (error) {
+        return console.log('Unable to connect to database!')   
+    }
+    
+    const db = client.db(databaseName)
+
+    db.collection('tasks').deleteOne({
+        description: 'task 1'
+    }).then((result) => {
+        console.log(result)
+    }).catch((error) => {
+        console.log(error)
+    })
+
+
+
+})
+
