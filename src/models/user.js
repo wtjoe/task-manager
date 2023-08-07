652a8b74a919dad7ec4c7e0dbb46d515f71b3c1b
Joe Robbins <joe.robbins@willowtreeapps.com>
2023-08-07 05:34:50 -0400

    router and schema

--- src/models/user.js
+++ src/models/user.js
@@ -1,45 +1,62 @@
 const mongoose = require ('mongoose')
 const validator = require('validator')
+const bcrypt = require('bcryptjs')
 
-const User = mongoose.model ('User', {
-    name: {
-        type: String,
-        required: true,
-        trim:true
-    }, 
-    email : {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid')
+const userSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim:true
+        }, 
+        email : {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value) {
+                if (!validator.isEmail(value)) {
+                    throw new Error('Email is invalid')
+                }
             }
-        }
-
-    },
-    age: {
-        type: Number, 
-        default: 0,
-        validate(value)  {
-            if (value < 0) {
-                throw new Error('Age must be a positive number')
+    
+        },
+        age: {
+            type: Number, 
+            default: 0,
+            validate(value)  {
+                if (value < 0) {
+                    throw new Error('Age must be a positive number')
+                }
             }
-        }
-    },
-    password: {
-        type: String,
-        required: true, 
-        trim: true,
-        minlength: 7,
-        validate(value) {
-            if(value.toLowerCase().includes('password', value)) {
-                throw new Error('Password cannot contain "password"')
+        },
+        password: {
+            type: String,
+            required: true, 
+            trim: true,
+            minlength: 7,
+            validate(value) {
+                if(value.toLowerCase().includes('password', value)) {
+                    throw new Error('Password cannot contain "password"')
+                }
             }
         }
     }
+)
+
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+
+    next()
+
 })
+
+const User = mongoose.model ('User', userSchema)
 
 
 
