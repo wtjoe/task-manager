a511b0a9abf29e3acb9055dd1ecf21f9cc3fd5d5
Joe Robbins <joe.robbins@willowtreeapps.com>
2023-08-15 05:27:20 -0400

    login endpoint

--- src/models/user.js
+++ src/models/user.js
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema(
         }, 
         email : {
             type: String,
+            unique: true,
             required: true,
             trim: true,
             lowercase: true,
@@ -44,6 +45,27 @@ const userSchema = new mongoose.Schema(
     }
 )
 
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user = await User.findOne({ email })
+
+    if (!user) {
+        throw new Error('unable to login')
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if (!isMatch) {
+        throw new Error('Unable to login')
+    }
+
+    return user
+
+}
+
+
+
+
+// Hash the plain text password before saving
 userSchema.pre('save', async function (next) {
     const user = this
 
