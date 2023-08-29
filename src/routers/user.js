5886b159f16bdec0fe62b901e81a6f079cd97213
Joe Robbins <joe.robbins@willowtreeapps.com>
2023-08-29 16:48:19 -0400

    Logout endpoints

--- src/routers/user.js
+++ src/routers/user.js
@@ -32,6 +32,34 @@ router.post('/users/login', async (req, res) => {
 
 })
 
+router.post('/users/logout', auth, async (req, res) => {
+    try {
+        req.user.tokens = req.user.tokens.filter((token) => {
+            return token.token !== req.token
+        })
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+        console.log(e)
+    }
+})
+
+router.post('/users/logoutALL', auth, async (req, res) => {
+    try {
+        req.user.tokens = []
+        await req.user.save()
+
+        res.send()
+    } catch(e) {
+        res.status(500).send()
+        console.log(e)
+    }
+})
+
+
+
 router.get('/users/me', auth, async (req, res) => {
     res.send(req.user)
 })
