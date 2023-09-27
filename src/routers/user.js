25db67a96c348d4b1897e405c96534a3fcb9935e
Joe Robbins <joe.robbins@willowtreeapps.com>
2023-09-27 06:24:04 -0400

    auth refactor for update user and delete user

--- src/routers/user.js
+++ src/routers/user.js
@@ -65,7 +65,7 @@ router.get('/users/me', auth, async (req, res) => {
 })
 
 
-router.patch('/users/:id', auth, async (req, res) => {
+router.patch('/users/me', auth, async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
     const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
@@ -75,19 +75,9 @@ router.patch('/users/:id', auth, async (req, res) => {
     }
 
     try {
-        // const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true})
-        
-        const user = await User.findById(req.params.id)
-
-        updates.forEach((update) => user[update] = req.body[update])
-
-        await user.save()
-
-        if(!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
+        updates.forEach((update) => req.user[update] = req.body[update])
+        await req.user.save()
+        res.send(req.user)
 
     } catch(e) {
         res.status(400).send(e)
@@ -97,16 +87,13 @@ router.patch('/users/:id', auth, async (req, res) => {
 
 router.delete('/users/me', auth, async (req, res) => {
     try {
-        const user = await User.findByIdAndDelete(req.params.id)
-        
-        if(!user) {
-            return res.status(404).send()
-        }
 
-        res.send(user)
+        await User.findOneAndDelete({ _id: req.user._id });
+        res.send(req.user)
 
     } catch(e) {
         res.status(500).send()
+        console.log(e)
     }
 })
 
